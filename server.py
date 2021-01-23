4ec20f553916b7c9b78ee09e39c23ff927afea41
michaelfswann <mikeswann@mail.com>
2021-01-23 16:51:01 +0000

    added some comments

--- server.py
+++ server.py
@@ -1,22 +1,32 @@
-# Python 3 server example
+# Python 3 server
+
+# http.server module defines classes (eg HTTPServer class and BaseHTTPRequestHandler base class) for implementing HTTP servers (Web servers)
+# see https://docs.python.org/3/library/http.server.html
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = "localhost"
 serverPort = 8080
 
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Hello world!</title></head>", "utf-8"))
+        self.wfile.write(
+            bytes("<html><head><title>Hello world!</title></head>", "utf-8")
+        )
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>Hello World!</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
-if __name__ == "__main__":        
+
+# module is being run as standalone module so python assigns __name__ as __main__
+
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
